fb74c8a248faf9b72c2c05046383633f9f113f2d
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-16 19:23:27 +0200

    Adds layout, header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
 
 function App() {
   return(
     <Router>
       <Routes>
-        <Route path="" element={}>
+        <Route path="/" element={ <Layout /> }>
 
         </Route>
       </Routes>
