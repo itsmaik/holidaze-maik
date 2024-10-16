7f77d141d747b2bd5bb02f01b0ab2b460172d0bd
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-16 13:10:19 +0200

    Install react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return(
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="" element={}>
+
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
