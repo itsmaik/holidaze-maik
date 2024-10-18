6a294fc86851025bef2aecfb0c3487dbe2c105fe
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-18 23:48:13 +0200

    Adds Homepage component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import VenueList from "../components/venues/VenueList"
+
+
+export default function Home () {
+  return (
+    <>
+      <section className="container mx-auto">
+        <VenueList />
+      </section>
+    </>
+  )
+}
