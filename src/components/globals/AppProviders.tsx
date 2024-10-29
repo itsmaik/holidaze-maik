9b0f326d36ed8766e909c6099de9cb02f2cf7033
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-29 15:59:17 +0100

    Installs queryDevtools

--- src/components/globals/AppProviders.tsx
+++ src/components/globals/AppProviders.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import  AuthProvider  from "@context/AuthContext";
 
 
@@ -9,6 +10,7 @@ export default function AppProviders ({children}) {
   return(
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <Router>
           {children}
         </Router>
