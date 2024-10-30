49baf7fea3fe3578defef36299f321bcbb3dd167
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-30 09:48:58 +0100

    Adds protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import AppProviders from "src/providers/AppProviders";
+import ProtectedRoute from "@components/auth/routes/ProtectedRoute";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home"
-import AppProviders from "@components/globals/AppProviders";
+import Login from "@components/auth/login/Login";
 
 function App() {
   return(
     <AppProviders>
       <Routes>
           <Route path="/" element={ <Layout /> }>
-            <Route index element={ <Home /> }></Route>
-
+            <Route index element={ <Home /> } />
+            <Route path="/login" element={ <Login />} />
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                {/* <Profile /> */}
+              </ProtectedRoute>
+            } />
+            
           </Route>
         </Routes>
     </AppProviders>
