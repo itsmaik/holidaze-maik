78dc9f96268dae080ab6c3830143aeaca44b8e06
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-11-05 22:16:57 +0100

    minor logic alterations

--- src/components/venues/templates/VenueListCard.tsx
+++ src/components/venues/templates/VenueListCard.tsx
@@ -1,10 +1,9 @@
-import { TVenueList } from "src/types/VenueTypes";
+import { TVenueList } from "src/types/venueTypes";
 import placeHolderImage from "src/assets/placeholder-img.avif";
-console.log(placeHolderImage);
 
 type TVenueListProps = {
   venue: TVenueList;
-}
+};
 
 export default function VenueListCard({ venue }: TVenueListProps) {
   return (
@@ -12,15 +11,19 @@ export default function VenueListCard({ venue }: TVenueListProps) {
       <div className="relative">
         <img
           src={venue.media.length > 0 ? venue.media[0].url : placeHolderImage}
-          alt={venue.media.length > 0 ? venue.media[0].alt || venue.name : venue.name}
+          alt={
+            venue.media.length > 0
+              ? venue.media[0].alt || venue.name
+              : venue.name
+          }
           onError={(e) => (e.target.src = placeHolderImage)}
           className="w-80 sm:w-96 h-72 object-cover rounded-md"
         />
 
         {venue.location && (
           <span className="absolute top-2 right-2 bg-white text-black text-sm font-bold px-2 py-1 rounded-full">
-          {venue.location.city ||= "oslo"}
-        </span>
+            {(venue.location.city ||= "oslo")}
+          </span>
         )}
       </div>
     </div>
