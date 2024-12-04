bf8924a680720779ea76f409ccae4f722f4570cc
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-04 10:25:29 +0100

    Adds logic for UI change on isLoggedIn state

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,37 +1,63 @@
-import Button from "@components/globals/Button"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "@hooks/useAuth";
+import Button from "@components/globals/Button";
 import Modal from "@components/globals/Modal";
 import Login from "@components/auth/login/Login";
-import { useState } from "react"
 import Register from "@components/auth/register/Register";
+import SearchBarForm from "../searchBar/SearchBarForm";
 
-export default function Navbar () {
+export default function Navbar() {
   const [isLoginOpen, setLoginOpen] = useState(false);
   const [isRegisterOpen, setRegisterOpen] = useState(false);
 
+  const { isLoggedIn, logout } = useAuth();
+
   return (
-    <nav className="relative w-full h-96 bg-cover" style={{backgroundImage: `url('/src/assets/bg-img-1.avif')` }}>
+    <nav
+      className='relative w-full h-96 bg-cover'
+      style={{ backgroundImage: `url('/src/assets/bg-img-1.avif')` }}
+    >
+      <div className='relative z-10 flex items-center justify-between p-6'>
+        <Link to={`/`}>
+          <div className='text-black font-bold text-2xl'>Holidaze</div>
+        </Link>
 
-      <div className="relative z-10 flex items-center justify-between p-6">
-        <div className="text-black font-bold text-2xl">Holidaze</div>
+        {!isLoggedIn ? (
+          <div className='flex space-x-2 text-black'>
+            <Button onClick={() => setLoginOpen(true)}> Login </Button>
+            <Button onClick={() => setRegisterOpen(true)}> Register </Button>
+          </div>
+        ) : (
+          <div className='flex space-x-2 text-black'>
+            <Button as={Link} to='/Profile'>
+              {" "}
+              Profile{" "}
+            </Button>
+            <Button onClick={logout}> Logout </Button>
+          </div>
+        )}
 
-        <div className="flex space-x-2 text-black">
-          <Button onClick={() => setLoginOpen(true)}> Login </Button>
-          <Button onClick={() => setRegisterOpen(true)}> Register </Button>
-        </div>
-
-        <Modal isOpen={isLoginOpen} onClose={() => setLoginOpen(false)} title="Login with an existing account" >
-          <Login />
+        <Modal
+          isOpen={isLoginOpen}
+          onClose={() => setLoginOpen(false)}
+          title='Login with an existing account'
+        >
+          <Login onSuccess={() => setLoginOpen(false)} />
         </Modal>
 
-        <Modal isOpen={isRegisterOpen} onClose={() => setRegisterOpen(false)} title="Register a new user account" >
-          <Register />
+        <Modal
+          isOpen={isRegisterOpen}
+          onClose={() => setRegisterOpen(false)}
+          title='Register a new user account'
+        >
+          <Register onSuccess={() => setRegisterOpen(false)} />
         </Modal>
-
       </div>
 
-      <div className="relative z-1 flex items-center justify-center h-full px-6">
-        <h1 className="text-2xl bg-black text-white p-2">Calendar content here</h1>
+      <div className='relative z-1 flex items-center justify-center h-80 px-6'>
+        <SearchBarForm />
       </div>
     </nav>
-  )
+  );
 }
