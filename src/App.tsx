d50d336bb7b7f8ecdf138119909d7ee4e8e46962
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-04 10:23:14 +0100

    Adds Login and SingleVenue routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,29 @@ import { Routes, Route } from "react-router-dom";
 import AppProviders from "src/providers/AppProviders";
 import ProtectedRoute from "@components/auth/routes/ProtectedRoute";
 import Layout from "./components/layout/Layout";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Login from "@components/auth/login/Login";
+import SingleVenue from "@pages/SingleVenues";
+import SearchResults from "@components/searchBar/SearchResults";
+import "react-calendar/dist/Calendar.css";
 
 function App() {
-  return(
+  return (
     <AppProviders>
       <Routes>
-          <Route path="/" element={ <Layout /> }>
-            <Route index element={ <Home /> } />
-            <Route path="/login" element={ <Login />} />
-            <Route path="/profile" element={
-              <ProtectedRoute>
-                {/* <Profile /> */}
-              </ProtectedRoute>
-            } />
-            
-          </Route>
-        </Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/:id" element={<SingleVenue />} />
+          <Route path="/search-results" element={<SearchResults />} />
+          <Route
+            path='/profile'
+            element={<ProtectedRoute>{/* <Profile /> */}</ProtectedRoute>}
+          />
+        </Route>
+      </Routes>
     </AppProviders>
-  )
+  );
 }
 
 export default App;
