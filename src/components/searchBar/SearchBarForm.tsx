b9f22bacc12d5fdfa80f2c2211666ac9544c0d06
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-04 10:24:34 +0100

    Adds SearchBar form and SearchBaResults

---
+++ src/components/searchBar/SearchBarForm.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react"
+import Button from "@components/globals/Button"
+import { useNavigate } from "react-router-dom";
+
+
+export default function SearchBarForm () {
+  const initialFormValues = {
+    location: "",
+    checkIn: "",
+    checkOut: "",
+    guests: 1,
+  };
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]: value});
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const query = new URLSearchParams(
+      Object.entries(formValues).reduce((acc, [key, value]) => {
+        if (value) acc[key] = value.toString(); // Add only non-empty values
+        return acc;
+      }, {} as Record<string, string>)
+    ).toString();
+
+    navigate(`/search-results?${query}`);
+    setFormValues(initialFormValues);
+  }
+
+  const isFormValid = formValues.location.trim().length > 0;
+
+  return(
+    <>
+      <div className="container flex flex-col w-full items-center">
+        <div className="">
+          <h1 className="font-bold text-4xl mb-2 text-white">Find your next destination</h1>
+        </div>
+        
+        <form onSubmit={handleSubmit} className="flex flex-row">
+
+          <div className="flex flex-col">
+            <label htmlFor="location">Location</label>
+            <input type="text" id="location" name="location" value={formValues.location} onChange={handleChange} aria-label="Search location"
+            placeholder="City, country, continent..."  className="w-full px-4 py-2 focus:outline-none rounded-s-md border-r"/>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="checkIn">Check-In</label>
+            <input type="date" id="checkIn" name="checkIn" value={formValues.checkIn} onChange={handleChange} aria-label="Check-in date" className="w-full px-4 py-2 focus:outline-none h-10 border-r"/>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="checkOut">Check-Out</label>
+            <input type="date" id="checkOut" name="checkOut" value={formValues.checkOut} onChange={handleChange} aria-label="Check-out date" className="w-full px-4 py-2 focus:outline-none h-10 border-r"/>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="guests">Guests</label>
+            <input type="text" id="guests" name="guests" value={formValues.guests} onChange={handleChange} aria-label="Number of guests" className="w-full px-4 py-2 focus:outline-none rounded-e-md"/>
+          </div>
+          <div className="flex flex-col self-end ms-2">
+            <Button type="submit" disabled={!isFormValid} className="bg-transparent text-white"> Search</Button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+};
---
+++ src/components/searchBar/SearchResults.tsx
@@ -0,0 +1,50 @@
+import { useSearchParams } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import VenueListCard from "@components/venues/templates/VenueListCard";
+import { TVenueList } from "src/types/venueTypes";
+
+export default function SearchResults() {
+  const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
+  const cachedVenues = queryClient.getQueryData<TVenueList[]>(["venues"]);
+
+  const filteredVenues = cachedVenues?.filter((venue) => {
+    const matchesLocation = searchParams.get("location")
+      ? venue.location.city
+          .toLowerCase()
+          .includes(searchParams.get("location")!.toLowerCase())
+      : true;
+
+    const matchesCheckIn = searchParams.get("checkIn")
+      ? new Date(searchParams.get("checkIn")!) >= new Date(venue.availableFrom)
+      : true;
+
+    const matchesCheckOut = searchParams.get("checkOut")
+      ? new Date(searchParams.get("checkOut")!) <= new Date(venue.availableUntil)
+      : true;
+
+    const matchesGuests = searchParams.get("guests")
+      ? parseInt(searchParams.get("guests")!, 10) <= venue.maxGuests
+      : true;
+
+    return matchesLocation && matchesCheckIn && matchesCheckOut && matchesGuests;
+  });
+  
+
+  return (
+    <>
+      <section className='container mx-auto'>
+        <h1 className="text-2xl font-bold mb-4">Search Results</h1>
+        {filteredVenues && filteredVenues.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+            {filteredVenues.map((venue) => (
+              <VenueListCard key={venue.id} venue={venue} />
+            ))}
+          </div>
+          ) : (
+          <p>No results found.</p>
+        )}
+      </section>
+    </>
+  );
+}
