44c9ef4abd34084a48c28a293d87821c67519d40
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-04 10:26:54 +0100

    Accepts onSuccess Props in order to control modal

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -3,19 +3,22 @@ import { useAuth } from "@hooks/useAuth";
 import { loginUser } from "@api/services/AuthServices";
 import LoginForm from "./LoginForm";
 import toast from "react-hot-toast";
-import { TLoginFormInput } from "src/types/loginTypes"
+import type { TLoginFormInput } from "src/types/loginTypes";
 
+type TLoginProps = {
+  onSuccess: () => void;
+}
 
-export default function Login () {
+export default function Login({ onSuccess }: TLoginProps) {
   const { login } = useAuth();
-  const [ error, setError ] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleLogin = async (data: TLoginFormInput)  => {
+  const handleLogin = async (data: TLoginFormInput) => {
     try {
       const response = await loginUser(data.email, data.password);
-      login(response.user, response.token)
+      login(response.email, response.accessToken);
       setError(null);
-      if (response) return toast.success("Login Successful");
+      if (response) return toast.success("Login Successful") && onSuccess();
     } catch (err) {
       if (err instanceof Error)
         setError("You have entered an invalid email or password");
@@ -26,5 +29,5 @@ export default function Login () {
     <>
       <LoginForm onSubmit={handleLogin} apiError={error} />
     </>
-  )
-};
+  );
+}
