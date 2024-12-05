b08968a2832129a5ba27c8adcdae94807993605c
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-05 22:06:58 +0100

    Updates createVenue form to fit screen size

--- src/components/globals/Modal.tsx
+++ src/components/globals/Modal.tsx
@@ -18,7 +18,7 @@ export default function Modal({
 
   return (
     <div className='fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50'>
-      <div className='bg-white rounded-lg p-6 w-full max-w-md mx-4'>
+      <div className='bg-white rounded-lg p-6 w-full max-w-md mx-4 flex flex-col'>
         <div className='flex justify-between mb-4'>
           <h2 className='text-xl font-semibold text-gray-600'>{title}</h2>
           <button
@@ -30,7 +30,9 @@ export default function Modal({
             <FaTimes />
           </button>
         </div>
-        <div className='mt-8'>{children}</div>
+        <div className='mt-8 overflow-y-auto' style={{maxHeight: 'calc(100vh - 200px)'}}>
+          {children}
+        </div>
       </div>
     </div>
   );
