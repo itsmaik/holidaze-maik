56848d3bd27ee4369f8d354e3af6f486979ac209
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-06 23:04:57 +0100

    adds user upcoming bookings logic, layout and styles

---
+++ src/components/profile/MyBookingsList.tsx
@@ -0,0 +1,10 @@
+import MyBookingsTable from "./MyBookingsTable";
+
+export default function MyBookingsList({bookings}) {
+  return (
+    <div className="max-w-4xl mx-auto mt-10 p-4 bg-white shadow-lg rounded-lg">
+      <h1 className="text-xl font-semibold text-gray-800">Upcoming Bookings</h1>
+      <MyBookingsTable bookings={bookings} />
+    </div>
+  );
+}
