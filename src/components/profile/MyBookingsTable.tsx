5865a58e653ce0cf676d9337db727ad0bd0cd3f6
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-08 21:26:13 +0100

    updates file locations

--- src/components/profile/MyBookingsTable.tsx
+++ src/components/profile/MyBookingsTable.tsx
@@ -1,10 +1,10 @@
-import TableHeader from "./TableHeader";
+import TableHeader from "../globals/TableHeader";
 import MyBookingsTableBody from "./MyBookingsTableBody";
 
 export default function MyBookingsTable({ headers, bookings }) {
   return (
-    <div className="overflow-x-auto mt-6">
-      <table className="min-w-full border border-gray-200 bg-white rounded-lg shadow-lg">
+    <div className='overflow-x-auto mt-6'>
+      <table className='min-w-full border border-gray-200 bg-white rounded-lg shadow-lg'>
         <>
           <TableHeader headers={headers} />
           <MyBookingsTableBody bookings={bookings} />
--- src/components/venues/templates/OwnerBookingCard.tsx
+++ src/components/venues/templates/OwnerBookingCard.tsx
@@ -1,8 +1,8 @@
 import type { TBooking } from "src/types/bookingTypes";
 import Button from "@components/globals/Button";
 import useDeleteVenue from "../actions/delete-venue/useDelete";
-import TableHeader from "@components/profile/TableHeader";
-import BookingsTableBody from "@components/profile/BookingsTableBody";
+import TableHeader from "@components/globals/TableHeader";
+import BookingsTableBody from "@components/venues/bookings/BookingsTableBody";
 import { bookingsHeaders } from "@utils/functions/bookingHeaders";
 import EditVenueModal from "../actions/edit-venue/EditVenueModal";
 import { TEditVenueProps } from "src/types/editVenueTypes";
@@ -15,7 +15,7 @@ type Props = {
 
 export default function OwnerBookingCard({ venue, venueId, bookings }: Props) {
   // const id = venueId;
-  const {deleteVenue} = useDeleteVenue();
+  const { deleteVenue } = useDeleteVenue();
 
   const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete this venue?")) {
@@ -24,21 +24,23 @@ export default function OwnerBookingCard({ venue, venueId, bookings }: Props) {
   };
 
   return (
-    <div className="max-w-5xl mx-auto mt-10 p-4 bg-white shadow-lg rounded-lg">
-      <h1 className="text-xl font-semibold text-gray-800">Customers bookings for this venue</h1>
-    
-      <div className="overflow-x-auto mt-6">
-        <table className="min-w-full border border-gray-200 bg-white rounded-lg shadow-lg">
+    <div className='max-w-5xl mx-auto mt-10 p-4 bg-white shadow-lg rounded-lg'>
+      <h1 className='text-xl font-semibold text-gray-800'>
+        Customers bookings for this venue
+      </h1>
+
+      <div className='overflow-x-auto mt-6'>
+        <table className='min-w-full border border-gray-200 bg-white rounded-lg shadow-lg'>
           <>
             <TableHeader headers={bookingsHeaders} />
             <BookingsTableBody bookings={bookings} />
           </>
         </table>
       </div>
-      <div className="flex flex-row mt-10 justify-center gap-8">
+      <div className='flex flex-row mt-10 justify-center gap-8'>
         <Button onClick={handleDelete}>Delete venue</Button>
-        <EditVenueModal venue={venue}  />
-        </div>
+        <EditVenueModal venue={venue} />
       </div>
+    </div>
   );
 }
