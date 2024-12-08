ac39c98484edb0b1ff5ee52c17d16c99003cccb2
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-12-09 00:47:42 +0100

    Makes changes to fix bug with paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-// https://vitejs.dev/config/
+// Simulate __dirname in ESM
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Vite configuration
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-})
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, './src/components'),
+    },
+  },
+});
