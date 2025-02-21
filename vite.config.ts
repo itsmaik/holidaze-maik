54f39711f26e1188f1a31e539a2bd935cf27f730
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2025-02-21 12:30:44 +0100

    Major dependencies updates. React, TW, React router, etc.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-// Simulate __dirname in ESM
+// Define __dirname in an ESM environment
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Vite configuration
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
   resolve: {
     alias: {
-      '@components': resolve(__dirname, './src/components'),
+      "@components": resolve(__dirname, "src/components"),
     },
   },
 });
