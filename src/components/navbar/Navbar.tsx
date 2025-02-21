d5caa76f4a52a5e54d9d5de65e729c66515114cd
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2025-02-21 12:41:44 +0100

    adds strict mode back

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,21 +10,19 @@ import { useAuth } from "@hooks/useAuth";
 export default function Navbar() {
   const location = useLocation();
   const isProfilePage = location.pathname === "/profile";
-  const {isLoggedIn} = useAuth();
+  const { isLoggedIn } = useAuth();
 
   return (
     <nav
-      className={`relative w-full ${
-        isProfilePage ? "h-20" : "h-96"
-      } bg-cover`}
+      className={`relative w-full ${isProfilePage ? "h-20" : "h-96"} bg-cover`}
       style={{ backgroundImage: `url(${heroImg})` }}
     >
-      <div className="relative z-10 flex items-center justify-between p-6">
-        <Link to="/">
-          <div className="text-black font-bold text-2xl">Holidaze</div>
+      <div className='relative z-10 flex items-center justify-between p-6'>
+        <Link to='/'>
+          <div className='text-black font-bold text-2xl'>Holidaze</div>
         </Link>
 
-        <div className="flex items-center gap-4">
+        <div className='flex items-center gap-4'>
           {/* Conditional Rendering for Auth Buttons */}
           {!isLoggedIn ? (
             <>
@@ -42,7 +40,7 @@ export default function Navbar() {
 
       {/* Search Bar for Non-Profile Pages */}
       {!isProfilePage && (
-        <div className="relative z-1 flex flex-col items-center justify-center h-80 px-6">
+        <div className='relative z-1 flex flex-col items-center justify-center h-80 px-6'>
           <SearchBarForm />
         </div>
       )}
